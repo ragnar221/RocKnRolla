24b2bdd28a1514483024c9cdbeacf957e8c9c071
juan sebastian viedma <juanse070991@gmail.com>
2023-11-21 21:45:33 -0300

    arregladaUrl

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 export const INITIAL_LIMIT = 3;
 export const SHIPPING_COST = 500;
-export const BASE_URL = 'http://rocknrolla.vercel.app/';
+export const BASE_URL = 'https://rocknrolla.vercel.app/';
