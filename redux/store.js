8bef4a26d14e4480bff378af632dc900ad4efab3
facundo <juanse070991@gmail.com>
2024-01-02 20:09:51 -0300

    storeFix

--- redux/store.js
+++ redux/store.js
@@ -3,15 +3,39 @@ import { persistStore } from "redux-persist";
 import rootReducer from "./rootReducer";
 import thunk from "redux-thunk";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import offersReducer from "./Offers/OffersReducer";
+import categoriesReducer from "./categories/categoriesReducer";
+import productsReducer from "./products/productsReducer";
+import cartReducer from "./cart/cartReducer";
+import userSlice from "./user/userSlice";
+import OrdersSlice from "./Orders/OrdersSlice";
+import recipesReducer from "./recipes/recipesReducer";
+import persistReducer from "redux-persist/es/persistReducer";
 
 
+const rootReducer = combineReducers({
+  ofertas: offersReducer,
+  categories: categoriesReducer,
+  products: productsReducer,
+  cart: cartReducer,
+  user: userSlice,
+  orders: OrdersSlice,
+  recetas: recipesReducer,
+});
 
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["cart", "user"],
+};
 
 const initialState = {};
 const middleware = [thunk];
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore( {
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware ({
     serializableCheck: false
   }) 
